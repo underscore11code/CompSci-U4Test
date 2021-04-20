1333bb2a6ad58e859e31b6a21f53e6d8ffe94e1e
underscore11code <minecrafter11mrt@gmail.com>
2021-04-20 09:15:49 -0700

    Guess

---
+++ src/main/java/io/github/underscore11code/compsci/Guess.java
@@ -0,0 +1,30 @@
+package io.github.underscore11code.compsci;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Guess {
+  public static final String[] animals = {
+          "dog",
+          "cat",
+          "pig",
+          "cow",
+          "chicken"
+  };
+
+  public static void main(String[] args) {
+    System.out.println("Welcome to Guess! See if you can guess my favourite animal!");
+    final String animal = animals[new Random().nextInt(animals.length-1)];
+    final Scanner scanner = new Scanner(System.in);
+    while (true) {
+      System.out.print("> ");
+      final String in = scanner.nextLine();
+      if (animal.equalsIgnoreCase(in.trim())) {
+        break;
+      } else {
+        System.out.println("Incorrect. Try again.");
+      }
+    }
+    System.out.println("You guessed it! Congrats!");
+  }
+}
--- src/main/java/io/github/underscore11code/compsci/Main.java
+++ src/main/java/io/github/underscore11code/compsci/Main.java
@@ -2,6 +2,6 @@ package io.github.underscore11code.compsci;
 
 public class Main {
   public static void main(String[] args) {
-    // Do shit here
+    Guess.main(args);
   }
 }
